d74c6246b038921bbf8987ab9d2401cbb687bc45
Ashish Bansal <bansal.ashish096@gmail.com>
2015-08-24 14:38:36 +0530

    Implemented DownloadModel class to create model/view architecture

---
+++ downloadmodel.cpp
@@ -0,0 +1,172 @@
+/*
+ * Copyright 2015 Ashish Bansal<bansal.ashish096@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+#include "downloadmodel.h"
+
+#include <QDebug>
+
+DownloadModel::DownloadModel() : QAbstractTableModel()
+{
+
+}
+
+int DownloadModel::rowCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent);
+    return items.size();
+}
+
+int DownloadModel::columnCount(const QModelIndex& parent) const
+{
+    Q_UNUSED(parent);
+    return TableView::END;
+}
+
+QVariant DownloadModel::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid()) {
+        return QVariant();
+    }
+
+    if (index.row() >= items.size() || index.row() < 0) {
+        return QVariant();
+    }
+
+    if (role == Qt::DisplayRole) {
+        TableItem item = items.at(index.row());
+        return item.get(index.column());
+    }
+
+    return QVariant();
+}
+
+QVariant DownloadModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (role != Qt::DisplayRole) {
+        return QVariant();
+    }
+
+    if (orientation == Qt::Horizontal) {
+        switch (section) {
+        case TableView::RowId :
+            return tr("Id");
+        case TableView::FileName :
+            return tr("Filename");
+        case TableView::FileSize :
+            return tr("Size");
+        case TableView::DownloadProgress :
+            return tr("Download Progress");
+        case TableView::TransferRate :
+            return tr("Transfer Rate");
+        case TableView::Status :
+            return tr("Status");
+        case TableView::TimeRemaining :
+            return tr("Time Remaining");
+        case TableView::DateAdded :
+            return tr("Date Added");
+        default:
+            return QVariant();
+        }
+    }
+    return QVariant();
+}
+
+Qt::ItemFlags DownloadModel::flags(const QModelIndex &index) const
+{
+    if (!index.isValid()) {
+        return Qt::ItemIsEnabled;
+    }
+    return QAbstractTableModel::flags(index);
+}
+
+bool DownloadModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+    if (index.isValid() && role == Qt::EditRole) {
+        int row = index.row();
+
+        TableItem item = items.value(row);
+
+        switch (index.column()) {
+            case TableView::RowId :
+                item.id = value.toInt();
+                break;
+            case TableView::FileName :
+                item.filename = value.toString();
+                break;
+            case TableView::FileSize :
+                item.filesize = value.toString();
+                break;
+            case TableView::DownloadProgress :
+                item.downloadProgress = value.toString();
+                break;
+            case TableView::TransferRate :
+                item.transferRate = value.toString();
+                break;
+            case TableView::Status :
+                item.status = value.toString();
+                break;
+            case TableView::TimeRemaining :
+                item.timeRemaining = value.toString();
+                break;
+            case TableView::DateAdded :
+                item.dateAdded = value.toString();
+                break;
+            default:
+                return false;
+        }
+        items.replace(row, item);
+        emit(dataChanged(index, index));
+
+        return true;
+    }
+    return false;
+}
+
+bool DownloadModel::insertRows(int position, int rows, const QModelIndex &index)
+{
+    Q_UNUSED(index);
+    beginInsertRows(QModelIndex(), position, position+rows-1);
+
+    for (int row=0; row < rows; row++) {
+        TableItem item;
+        items.insert(position, item);
+    }
+
+    endInsertRows();
+    return true;
+}
+
+bool DownloadModel::removeRows(int position, int rows, const QModelIndex &index)
+{
+    Q_UNUSED(index);
+    beginRemoveRows(QModelIndex(), position, position+rows-1);
+
+    for (int row = 0; row < rows; ++row) {
+        items.removeAt(position);
+    }
+
+    endRemoveRows();
+    return true;
+}
+
+QList< DownloadModel::TableItem > DownloadModel::getList()
+{
+    return items;
+}
---
+++ downloadmodel.h
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2015 Ashish Bansal<bansal.ashish096@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+#ifndef DOWNLOADMODEL_H
+#define DOWNLOADMODEL_H
+
+#include <enum.h>
+
+#include <QObject>
+#include <QAbstractTableModel>
+#include <QVariant>
+#include <QList>
+
+class DownloadModel : public QAbstractTableModel
+{
+    Q_OBJECT
+public:
+    DownloadModel();
+    int rowCount(const QModelIndex &parent) const;
+    int columnCount(const QModelIndex &parent) const;
+    QVariant data(const QModelIndex &index, int role) const;
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+    Qt::ItemFlags flags(const QModelIndex &index) const;
+    bool setData(const QModelIndex &index, const QVariant &value, int role=Qt::EditRole);
+    bool insertRows(int position, int rows, const QModelIndex &index=QModelIndex());
+    bool removeRows(int position, int rows, const QModelIndex &index=QModelIndex());
+
+    struct TableItem{
+        qint64 id;
+        qint64 rowId;
+        QString filename;
+        QString filesize;
+        QString downloadProgress;
+        QString transferRate;
+        QString status;
+        QString timeRemaining;
+        QString dateAdded;
+        QString resumeCapability;
+
+        QVariant get(int column)
+        {
+            switch (column) {
+                case TableView::RowId :
+                    return rowId;
+                case TableView::FileName :
+                    return filename;
+                case TableView::FileSize :
+                    return filesize;
+                case TableView::DownloadProgress :
+                    return downloadProgress;
+                case TableView::TransferRate :
+                    return transferRate;
+                case TableView::Status :
+                    return status;
+                case TableView::TimeRemaining :
+                    return timeRemaining;
+                case TableView::DateAdded :
+                    return dateAdded;
+                default :
+                    return QVariant();
+            };
+        }
+    };
+
+    QList< TableItem > getList();
+    QList< TableItem > items;
+};
+
+#endif // DOWNLOADMODEL_H
